53e9fbf4f21c960f96451484038aecf4d34713c9
bpathade <92673871+bpathade@users.noreply.github.com>
2021-12-27 20:08:30 +0530

    first commit

---
+++ shape.js
@@ -0,0 +1,18 @@
+<html>
+  <body>
+
+    <script>
+      class Shape{ 
+        constructor(){ 
+          document.write("Class <i>Shape</i> created.")
+
+          this.name = "circle";
+          this.color = "black";
+          this.radius = 5; 
+        }
+      }
+      var shape1 = new Shape(); 
+    </script>
+
+  </body>
+</html>
